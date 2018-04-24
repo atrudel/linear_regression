ca2cb3d7be2af5c87c6d1d9aa4b8be401311e8ee
Amric TRUDEL <atrudel@e2r1p4.42.fr>
2018-04-24 13:49:53 +0200

    add readme and requirements

--- estimate.py
+++ estimate.py
@@ -6,9 +6,11 @@ import os
 import pickle
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Select visualization options")
+    parser = argparse.ArgumentParser(description="Select additional options")
+    parser.add_argument("-r", "--reset", action="store_true",
+                        help="Erase previously trained model")
     parser.add_argument("-v", "--visual", action="store_true",
-                    help="Show prediction in a graph among the dataset")
+                        help="Show prediction in a graph among the dataset")
     return parser.parse_args()
 
 def plot_estimation(mileage, price):
@@ -28,13 +30,18 @@ args = parse_args()
 print("Welcome.")
 print("This program can estimate the price of a car according to its mileage.")
 
+# Erase previously trained model if --reset option was selected
+if args.reset:
+    os.remove("model.pickle")
+    print("Deleting previous model...")
+
 # Recover previously trained model, or create a new one
 try:
     with open("model.pickle", 'rb') as f:
         model = pickle.load(f)
 except FileNotFoundError:
     model = LinearRegression()
-    print("(The program hasn't been trained yet...)")
+    print("(The model isn't trained...)")
 
 # Prompt user input and process it
 try:
